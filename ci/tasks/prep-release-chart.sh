bb836efbffbdd7586d19a0358c597ac440f10cbb
Sandipan Dey <hey@sandipan.dev>
2022-01-21 17:15:00 +0530

    fix: commit release to git

--- ci/tasks/prep-release-chart.sh
+++ ci/tasks/prep-release-chart.sh
@@ -14,15 +14,30 @@ echo ""
 # Strips off "-dev"
 bump2version pre --current-version $(cat version/version) --allow-dirty version/version
 
-# Update version in Chart.yml
 export version=$(cat version/version)
-yq e -i '.version = strenv(version)' repo/charts/$CHART/Chart.yaml
-
 echo "Release Version: $version"
 
-# Getting TGZ files for release
 mkdir -p gh-release/files
+
 pushd repo
+
+  # Update version in Chart.yml
+  yq e -i '.version = strenv(version)' charts/$CHART/Chart.yaml
+
+  # Setup Config
+  if [[ -z $(git config --global user.email) ]]; then
+    git config --global user.email "bot@galoy.io"
+  fi
+  if [[ -z $(git config --global user.name) ]]; then
+    git config --global user.name "CI Bot"
+  fi
+
+  # Add change to Git
+  git add charts/$CHART/Chart.yaml
+  git status
+  git commit -m "ci(release): $CHART release v$version"
+
+  # Getting TGZ files for release
   cr package charts/$CHART
   cp -R .cr-release-packages ../gh-release/files
 popd
