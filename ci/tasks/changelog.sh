75029c2f31a42747369f48d12a89c1e474fd7363
Sandipan Dey <hey@sandipan.dev>
2022-01-21 11:35:09 +0530

    fix: indirect expansion cross shell

--- ci/tasks/changelog.sh
+++ ci/tasks/changelog.sh
@@ -5,15 +5,16 @@ set -eu
 . meta-release/ci/tasks/refs.sh
 
 export new_ref=$(cat $new_git_ref_path)
+export prev_ref=$(eval "echo \$$depl_name")
 
 echo $new_ref > release-commitid/commitid
 
-if [[ $new_ref == ${!depl_name} ]]; then
+if [[ $new_ref == $prev_ref ]]; then
   echo "Refs didn't change, changelog will be empty..."
   echo "" > changelog/changelog
 else
   pushd repo
-    git cliff --config ../meta-release/config.toml ${!depl_name}..$new_ref > ../changelog/changelog
+    git cliff --config ../meta-release/config.toml $prev_ref..$new_ref > ../changelog/changelog
   popd
 fi
 
