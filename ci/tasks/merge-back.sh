a128652b23b246912cbdf3e9d680fd8f2153ded1
Sandipan Dey <hey@sandipan.dev>
2022-01-21 19:58:20 +0530

    chore: change merge commit message

--- ci/tasks/merge-back.sh
+++ ci/tasks/merge-back.sh
@@ -9,10 +9,12 @@ if [[ -z $(git config --global user.name) ]]; then
   git config --global user.name "CI Bot"
 fi
 
+export version=$(cat version/version)
+
 pushd repo
 
 git checkout main
 git status
-git merge -X theirs --no-edit release
+git merge -X theirs -m "ci: Update $CHART dev version to $version" release
 git status
 git log -n 5
