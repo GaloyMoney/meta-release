3cb28c09460bc75196b40addd1625ed9aeb8546a
Sandipan Dey <hey@sandipan.dev>
2022-01-21 11:22:10 +0530

    fix: rename repo -> meta-release

--- ci/tasks/refs.sh
+++ ci/tasks/refs.sh
@@ -3,22 +3,22 @@
 set -eu
 
 # Galoy
-export galoy=$(yq e '.galoy' repo/release.yml)
+export galoy=$(yq e '.galoy' meta-release/release.yml)
 
 # Infra
-export infra=$(yq e '.infra.commit' repo/release.yml)
+export infra=$(yq e '.infra.commit' meta-release/release.yml)
 
 # Bitcoind
-export bitcoind=$(yq e '.bitcoind' repo/release.yml)
-export lnd=$(yq e '.lnd' repo/release.yml)
-export rtl=$(yq e '.rtl' repo/release.yml)
-export specter=$(yq e '.specter' repo/release.yml)
+export bitcoind=$(yq e '.bitcoind' meta-release/release.yml)
+export lnd=$(yq e '.lnd' meta-release/release.yml)
+export rtl=$(yq e '.rtl' meta-release/release.yml)
+export specter=$(yq e '.specter' meta-release/release.yml)
 
 # Monitoring
-export monitoring=$(yq e '.monitoring' repo/release.yml)
-export lndmon=$(yq e '.lndmon' repo/release.yml)
+export monitoring=$(yq e '.monitoring' meta-release/release.yml)
+export lndmon=$(yq e '.lndmon' meta-release/release.yml)
 
 # Addons
-export admin_panel=$(yq e '.admin_panel' repo/release.yml)
-export galoy_pay=$(yq e '.galoy_pay' repo/release.yml)
-export dealer=$(yq e '.dealer' repo/release.yml)
+export admin_panel=$(yq e '.admin_panel' meta-release/release.yml)
+export galoy_pay=$(yq e '.galoy_pay' meta-release/release.yml)
+export dealer=$(yq e '.dealer' meta-release/release.yml)
