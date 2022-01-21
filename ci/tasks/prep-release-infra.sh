8d452a69684b1886d0a81c9a17a6fec2f4d622d1
Sandipan Dey <sandipandey.12@gmail.com>
2022-01-21 11:20:15 +0530

    chore: cleanup infra release phase (#13)
    * chore: Changelog generation standardized
    
    * Removed push to meta release, cleanup prep-release
    
    * fix: removed charts traces

--- ci/tasks/prep-release-infra.sh
+++ ci/tasks/prep-release-infra.sh
@@ -3,7 +3,7 @@
 set -eu
 
 if ! [[ -f changelog/changelog ]]; then
-  echo "No changelog found, skipping..."
+  echo "No changelog found, shouldn't run without changelog, skipping..."
   exit 0
 fi
 
@@ -15,9 +15,21 @@ if [[ $(cat changelog/changelog | grep breaking) != '' ]] || [[ $(cat changelog/
   echo "Breaking change / Feature Addition found, bumping minor version..."
   bump2version minor --current-version $(cat version/version) --allow-dirty version/version
 else
-  echo "Only patches and fixes found - no breaking changes, bumping patch version..."
+  echo "Only patches and fixes found - no breaking changes or features, bumping patch version..."
   bump2version patch --current-version $(cat version/version) --allow-dirty version/version
 fi
 
 echo -n "New Version: "
 cat version/version
+echo ""
+
+export version=$(cat version/version)
+
+echo "v$version Release" > gh-release/name
+echo "v$version" > gh-release/tag
+cat changelog/changelog > gh-release/notes.md
+
+commitid=$(cat release-commitid/commitid)
+
+cd repo-stable
+git checkout $commitid
