76868e26b13843957847939a0470dd3baf0429c0
Sandipan Dey <hey@sandipan.dev>
2022-01-26 17:58:30 +0530

    fix: release version should be calculated first, bump is just -dev

--- ci/tasks/prep-release-chart.sh
+++ ci/tasks/prep-release-chart.sh
@@ -14,6 +14,15 @@ echo ""
 # Strips off "-dev"
 bump2version pre --current-version $(cat version/version) --allow-dirty version/version
 
+# Figure out proper version to release
+if [[ $(cat changelog/changelog | grep breaking) != '' ]] || [[ $(cat changelog/changelog | grep feature) != '' ]]; then
+  echo "Breaking change / Feature Addition found, bumping minor version..."
+  bump2version minor --current-version $(cat version/version) --allow-dirty version/version
+else
+  echo "Only patches and fixes found - no breaking changes, bumping patch version..."
+  bump2version patch --current-version $(cat version/version) --allow-dirty version/version
+fi
+
 export version=$(cat version/version)
 echo "Release Version: $version"
 
