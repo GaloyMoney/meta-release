d5bf967638b15fafd4038d882c0f81aa687c44a8
Sandipan Dey <hey@sandipan.dev>
2022-01-26 16:07:24 +0530

    revert: exchange patch and minor for infra release
    This reverts commit 9160fcf12772a01910e1f399fb5571755ea8d627.

--- ci/tasks/prep-release-infra.sh
+++ ci/tasks/prep-release-infra.sh
@@ -13,10 +13,10 @@ echo ""
 
 if [[ $(cat changelog/changelog | grep breaking) != '' ]] || [[ $(cat changelog/changelog | grep feature) != '' ]]; then
   echo "Breaking change / Feature Addition found, bumping minor version..."
-  bump2version patch --current-version $(cat version/version) --allow-dirty version/version
+  bump2version minor --current-version $(cat version/version) --allow-dirty version/version
 else
   echo "Only patches and fixes found - no breaking changes or features, bumping patch version..."
-  bump2version minor --current-version $(cat version/version) --allow-dirty version/version
+  bump2version patch --current-version $(cat version/version) --allow-dirty version/version
 fi
 
 echo -n "New Version: "
