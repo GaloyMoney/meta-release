2865278af23ec5259faf16f9b7116b7fd52aacc8
Sandipan Dey <hey@sandipan.dev>
2022-01-31 01:50:09 +0530

    ci: release source codes

---
+++ ci/tasks/prep-release-src.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -eu
+
+# ------------ CHANGELOG ------------
+
+# Fetching Refs
+export prev_ref=$(yq e '.image.git_ref' charts-repo/charts/$CHART/values.yaml)
+export new_ref=$(cd repo && git rev-parse HEAD)
+
+# Generate Changelog
+if [[ $new_ref == $prev_ref ]]; then
+  echo "Refs didn't change, changelog will be empty..."
+  echo "" > artifacts/gh-release-notes.md
+else
+  pushd repo
+    git cliff --config ../meta-release/config.toml $prev_ref..$new_ref > ../artifacts/gh-release-notes.md
+  popd
+fi
+
+echo "CHANGELOG:"
+echo "--------------------------------------------------"
+cat artifacts/gh-release-notes.md
+echo "--------------------------------------------------"
+
+# ------------ BUMP VERSION ------------
+
+echo -n "Prev Version: "
+cat version/version
+echo ""
+
+# Figure out proper version to release
+if [[ $(cat artifacts/gh-release-notes.md | grep breaking) != '' ]] || [[ $(cat artifacts/gh-release-notes.md | grep feature) != '' ]]; then
+  echo "Breaking change / Feature Addition found, bumping minor version..."
+  bump2version minor --current-version $(cat version/version) --allow-dirty version/version
+else
+  echo "Only patches and fixes found - no breaking changes, bumping patch version..."
+  bump2version patch --current-version $(cat version/version) --allow-dirty version/version
+fi
+
+echo -n "Release Version: "
+cat version/version
+echo ""
+
+# ------------ ARTIFACTS ------------
+
+cat version/version > artifacts/gh-release-tag
+echo "v$(cat version/version) Release" > artifacts/gh-release-name
