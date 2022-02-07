a862f2e666e9f90cb4543cfe1af9cd92821bed94
Sandipan Dey <hey@sandipan.dev>
2022-02-07 19:09:22 +0530

    fix: generate changelog from last published version instead of refs on meta-release (#30)
    * fix: changelog generation logic from last tag instead of from refs
    
    * fix: changelog for infra version tag fix

--- ci/tasks/changelog.sh
+++ ci/tasks/changelog.sh
@@ -2,10 +2,15 @@
 
 set -eu
 
-. meta-release/ci/tasks/refs.sh
-
 export new_ref=$(cat $new_git_ref_path)
-export prev_ref=$(eval "echo \$$(echo $depl_name | sed 's/-/_/g')")
+
+pushd repo
+if [[ $depl_name != 'infra' ]]; then
+  export prev_ref=$(git rev-list -n 1 $depl_name-v$(cat ../version/version))
+else
+  export prev_ref=$(git rev-list -n 1 $(cat ../version/version))
+fi
+popd
 
 echo $new_ref > release-commitid/commitid
 
