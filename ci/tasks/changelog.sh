0e0edb3f39b842cb1056da67e46304826f56d15f
Sandipan Dey <hey@sandipan.dev>
2022-02-07 19:08:12 +0530

    fix: changelog for infra version tag fix

--- ci/tasks/changelog.sh
+++ ci/tasks/changelog.sh
@@ -5,7 +5,11 @@ set -eu
 export new_ref=$(cat $new_git_ref_path)
 
 pushd repo
-export prev_ref=$(git rev-list -n 1 $depl_name-v$(cat ../version/version))
+if [[ $depl_name != 'infra' ]]; then
+  export prev_ref=$(git rev-list -n 1 $depl_name-v$(cat ../version/version))
+else
+  export prev_ref=$(git rev-list -n 1 $(cat ../version/version))
+fi
 popd
 
 echo $new_ref > release-commitid/commitid
