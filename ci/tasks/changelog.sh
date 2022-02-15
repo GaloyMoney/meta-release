2148d503ad0cc3373e870ab1763169501989ff3d
Sandipan Dey <hey@sandipan.dev>
2022-02-15 11:06:35 +0530

    ci: fix version tag format in infra

--- ci/tasks/changelog.sh
+++ ci/tasks/changelog.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -eu
+set -e
 
 export new_ref=$(cat $new_git_ref_path)
 echo $new_ref > release-commitid/commitid
@@ -21,7 +21,7 @@ else
   if [[ $depl_name != 'infra' ]]; then
     export prev_ref=$(git rev-list -n 1 $depl_name-v$(cat ../version/version))
   else
-    export prev_ref=$(git rev-list -n 1 $(cat ../version/version))
+    export prev_ref=$(git rev-list -n 1 v$(cat ../version/version))
   fi
   popd
 
